4dd4b4241ffc866f3240396f3ac06aae8081c60b
yyu-k <kiuyy@outlook.com>
2024-11-02 23:26:07 +0800

    Fixed - Warning: Cannot update a component (`Router`) while rendering a different component (`Page`). See https://stackoverflow.com/questions/72160276/warning-cannot-update-a-component-browserrouter-while-rendering-a-different

--- seoblog/frontend/app/page.js
+++ seoblog/frontend/app/page.js
@@ -1,18 +1,22 @@
 'use client'
 
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import Loading from '@/components/LoadingComponent';
 import { getLocalStorageUser } from '@/actions/auth';
 
 export default function Page() {
   const router = useRouter();
   const user = getLocalStorageUser();
-  if (!user) {
-    router.push('/signin');
-  } else if (user.role === 1) {
-    router.push('/admin')
-  } else {
-    router.push('/user')
-  }
+  //from https://stackoverflow.com/questions/72160276/warning-cannot-update-a-component-browserrouter-while-rendering-a-different
+  useEffect(()=>{
+    if (!user) {
+      router.push('/signin');
+    } else if (user.role === 1) {
+      router.push('/admin');
+    } else {
+      router.push('/user');
+    }
+  },[user])
   return <div className='container'><Loading/></div>
 }
